4ac596f63d99a95c177aee829748db53bcef4ec6
Vadim <isako-vadim@yandex.ru>
2026-09-21 17:45:07 +0500

    REFACTOR

--- src/main/resources/db/sql/createShedlockTable.sql
+++ src/main/resources/db/sql/createShedlockTable.sql
@@ -1,7 +1,6 @@
 CREATE TABLE shedlock (
-  name VARCHAR(64),
+  name VARCHAR(64) PRIMARY KEY,
   lock_until TIMESTAMP(3) NULL,
   locked_at TIMESTAMP(3) NULL,
-  locked_by VARCHAR(255),
-  PRIMARY KEY (name)
+  locked_by VARCHAR(255)
 )
